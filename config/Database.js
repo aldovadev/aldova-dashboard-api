7461a91c325e3b3d1b19d529a2ebadc29176d8ca
yunomi811 <aldova811@gmail.com>
2023-07-16 10:08:16 +0700

    remove password

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("aldovadb", "root", "Inandita99", {
+const db = new Sequelize("aldovadb", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
