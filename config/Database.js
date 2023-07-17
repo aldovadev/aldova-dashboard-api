cc43fa519f661d3bfaa06b378636fefac3da8989
yunomi811 <aldova811@gmail.com>
2023-07-17 22:21:58 +0700

    add dotenv library

--- config/Database.js
+++ config/Database.js
@@ -1,4 +1,6 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = new Sequelize("aldovadb", process.env.USER, process.env.PASSWORD, {
   host: "localhost",
