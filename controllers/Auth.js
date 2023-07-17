a778a2205e670f01f1987af1bcad686d6ddfe1aa
yunomi811 <aldova811@gmail.com>
2023-07-18 00:50:27 +0700

    testing allow control

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -15,6 +15,7 @@ export const Login = async (req, res) => {
   const name = user.name;
   const email = user.email;
   const role = user.role;
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.status(200).json({ uuid, name, email, role });
 };
 
